abb427d18cc95464a4a78c6d2f4926916388aed5
Samuel Cassiba <s@cassiba.com>
2016-07-13 08:02:52 -0700

    Fix run script

--- plans/keystone/plan.sh
+++ plans/keystone/plan.sh
@@ -5,7 +5,21 @@ pkg_maintainer="Samuel Cassiba <s@cassiba.com>"
 pkg_license=('apachev2')
 pkg_source=nosuchfile.tar.gz
 pkg_deps=(core/glibc core/python2 core/httpd core/pcre)
-pkg_build_deps=(core/coreutils core/curl core/git core/cacerts core/wget core/zlib core/openssl core/bzip2 core/sqlite core/make core/gcc core/libffi core/pcre)
+pkg_build_deps=(
+  core/bzip2
+  core/cacerts
+  core/coreutils
+  core/curl
+  core/gcc
+  core/git
+  core/libffi
+  core/make
+  core/openssl
+  core/pcre
+  core/sqlite
+  core/wget
+  core/zlib
+)
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
 pkg_expose=(5000 35357)
@@ -31,12 +45,12 @@ do_download() {
 }
 
 do_prepare() {
-  export LD_LIBRARY_PATH="$(pkg_path_for core/libffi)/lib:$(pkg_path_for core/pcre)/lib"
   export PIP_CERT="$(pkg_path_for core/cacerts)/ssl/certs/cacert.pem"
   export PYTHONPATH="${pkg_prefix}/lib/python2.7/site-packages:$(pkg_path_for core/python2)/lib/python2.7/site-packages"
 }
 
 do_build() {
+  export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(pkg_path_for core/libffi)/lib:$(pkg_path_for core/pcre)/lib"
   sed -i'' 's:#.*$::g' $HAB_CACHE_SRC_PATH/${pkg_name}/requirements.txt
   python -m ensurepip
   pip install --install-option="--prefix=${pkg_prefix}" pbr

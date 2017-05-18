a7086f5fe517749d88f5687960c6f67fd6898e77
Samuel Cassiba <samuel.cassiba@workday.com>
2017-05-18 15:01:15 -0700

    Keystone: refactor init hook, depend on hab pkgs for python

--- plans/keystone/plan.sh
+++ plans/keystone/plan.sh
@@ -5,7 +5,18 @@ pkg_maintainer="Samuel Cassiba <s@cassiba.com>"
 pkg_license=('apachev2')
 pkg_source=https://github.com/openstack/keystone/archive/${pkg_version}.tar.gz
 pkg_shasum=3cc7e03446ff2426849181b9ffdb8ea85591da62a3043abfa9ba441a77ee4651
-pkg_deps=(core/glibc core/python2 core/pcre)
+pkg_deps=(
+  core/glibc
+  core/iana-etc
+  core/mysql-client
+  core/pcre
+  python/python2
+  python/appdirs
+  python/packaging
+  python/setuptools
+  python/six
+  python/uwsgi
+)
 pkg_build_deps=(
   core/bzip2
   core/cacerts
@@ -13,13 +24,10 @@ pkg_build_deps=(
   core/curl
   core/gcc
   core/git
+  core/iana-etc
   core/libffi
   core/make
-  core/mysql
   core/openssl
-  core/pcre
-  core/rabbitmq
-  core/sqlite
   core/wget
   core/zlib
 )
@@ -31,9 +39,16 @@ do_prepare() {
   export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(pkg_path_for core/libffi)/lib:$(pkg_path_for core/pcre)/lib"
   export LD_RUN_PATH="$LD_RUN_PATH:$(pkg_path_for core/pcre)/lib"
   export PIP_CERT=`python -m pip._vendor.requests.certs`
-  export PYTHONPATH="${pkg_prefix}/lib/python2.7/site-packages:$(pkg_path_for core/python2)/lib/python2.7/site-packages"
+  export PYTHONPATH="${pkg_prefix}/lib/python2.7/site-packages:$(pkg_path_for python/python2)/lib/python2.7/site-packages"
   export SKIP_GIT_SDIST=1
   export PBR_VERSION="${pkg_version}"
+  # create symlinks for /etc/services and /etc/protocols
+  if [[ ! -f /etc/services ]]; then
+    cp -v $(pkg_path_for iana-etc)/etc/services /etc/services
+  fi
+  if [[ ! -f /etc/protocols ]]; then
+    cp -v $(pkg_path_for iana-etc)/etc/protocols /etc/protocols
+  fi
 }
 
 do_build() {
@@ -41,16 +56,14 @@ do_build() {
   wget -O $HAB_CACHE_SRC_PATH/get-pip.py https://bootstrap.pypa.io/get-pip.py
   python $HAB_CACHE_SRC_PATH/get-pip.py
   python -m ensurepip
-  pip install --install-option="--prefix=${pkg_prefix}" pbr
-  pip install --install-option="--prefix=${pkg_prefix}" -U setuptools
+  pip install --install-option="--prefix=${pkg_prefix}" -U pbr
   pip install --install-option="--prefix=${pkg_prefix}" -U vcversioner
-  pip install --install-option="--prefix=${pkg_prefix}" -U uwsgi
 }
 
 do_install() {
   pushd $HAB_CACHE_SRC_PATH/${pkg_name}-${pkg_version} > /dev/null
   pip install --install-option="--prefix=${pkg_prefix}" -r requirements.txt
-  $(pkg_path_for core/python2)/bin/python setup.py install --prefix="${pkg_prefix}"
+  $(pkg_path_for python/python2)/bin/python setup.py install --prefix="${pkg_prefix}"
   for egg in repoze paste
   do touch ${pkg_prefix}/lib/python2.7/site-packages/$egg/__init__.py
   done
